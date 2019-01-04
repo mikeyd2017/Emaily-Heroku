a6cd8b48e3b79dc86504b87d381e28d9ca591289
mikeyd2017 <dambrosiomichael0@gmail.com>
2019-01-04 17:33:05 -0600

    Test change on sendfile dirname

--- index.js
+++ index.js
@@ -25,12 +25,12 @@ require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
 if ( process.env.NODE_ENV === 'production') {
-    //Exoress will serve up production assets like our main.js file, or main.css file
+    //Express will serve up production assets like our main.js file, or main.css file
     app.use(express.static('client/build'));
     //Express will server up the index.html file if it doesn't recognize the route
     const path = require('path');
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
