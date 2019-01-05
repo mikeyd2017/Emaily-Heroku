6bea96328ab8f46811810e6e3375801e4e797ef9
mikeyd2017 <dambrosiomichael0@gmail.com>
2019-01-04 18:11:18 -0600

    Test change on sendfile dirname

--- config/keys.js
+++ config/keys.js
@@ -2,7 +2,6 @@
 if (process.env.NODE_ENV === 'production') {
     //we are in production - return the prod set of keys
     module.exports = require('./prod');
-    Stripe.setPublishableKey('pk_test_BHU8BnoRyVGTlqT1Qi88uOl1');
 } else {
     //we are in development - return the dev keys!
     module.exports = require('./dev');
