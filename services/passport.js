1d76bba48a9eabbeefdea793f8fdad98c30a8e75
Michael D'Ambrosio <dambrosiomichael@rocketmail.com>
2020-06-18 18:23:13 -0500

    Updating Google OAuth 2.0 Strategy

--- services/passport.js
+++ services/passport.js
@@ -1,5 +1,5 @@
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 const mongoose = require("mongoose");
 const keys = require("../config/keys");
 
